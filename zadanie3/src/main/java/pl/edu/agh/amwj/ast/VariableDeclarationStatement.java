0383e00c80af6f17399cbcacbbc63a848f45a3a1
Kurtzz <przemyslaw.kurc@gmail.com>
2016-11-10 17:08:49 +0100

    Add variable declaration statement

---
+++ zadanie3/src/main/java/pl/edu/agh/amwj/ast/VariableDeclarationStatement.java
@@ -0,0 +1,20 @@
+package pl.edu.agh.amwj.ast;
+
+import static pl.edu.agh.amwj.Data.variables;
+
+/**
+ * Created by Comarch on 2016-11-10.
+ */
+public class VariableDeclarationStatement implements Statement {
+    private String name;
+    private Expression value;
+
+    public VariableDeclarationStatement(String name, Expression value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public void execute() {
+        variables.put(name, value.evaluate());
+    }
+}
