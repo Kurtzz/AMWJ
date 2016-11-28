4f5b93335d923ae47b0af3957a9143c7698d2b4c
Kurtzz <przemyslaw.kurc@gmail.com>
2016-11-28 17:33:21 +0100

    Add size as program parameter

--- zadanie3/src/main/java/pl/edu/agh/amwj/Main.java
+++ zadanie3/src/main/java/pl/edu/agh/amwj/Main.java
@@ -1,8 +1,7 @@
 package pl.edu.agh.amwj;
 
-import pl.edu.agh.amwj.exceptions.InvalidVariableNameException;
-import pl.edu.agh.amwj.interpreter.Interpreter;
 import pl.edu.agh.amwj.exceptions.InvalidHeapSizeException;
+import pl.edu.agh.amwj.interpreter.Interpreter;
 
 import static pl.edu.agh.amwj.utils.FileReader.readFile;
 
@@ -12,20 +11,30 @@ import static pl.edu.agh.amwj.utils.FileReader.readFile;
 public class Main {
     public static void main(String[] args) {
         // Just show the usage and quit if a script wasn't provided.
-        if (args.length != 1) {
-            System.out.println("Usage: main <npj file path>");
+        if (args.length != 2) {
+            System.out.println("Usage: main -Dnpj.heap.size=<size> <npj file path>");
             return;
+        }
+        String path;
+        int size;
+
+        if (args[0].startsWith("-Dnpj.heap.size")) {
+            size = Integer.parseInt(args[0].split("=")[1]);
+            path = args[1];
+        } else {
+            size = Integer.parseInt(args[1].split("=")[1]);
+            path = args[0];
         }
 
         try {
-            Data.initializeData(24);
+            Data.initializeData(size);
         } catch (InvalidHeapSizeException e) {
             e.printStackTrace();
             return;
         }
 
         // Read the file.
-        String contents = readFile(args[0]);
+        String contents = readFile(path);
 
         // Run it.
         try {
