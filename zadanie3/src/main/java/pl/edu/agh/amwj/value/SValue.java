3211ca36ae90821a72b1402c752326e6000d8581
Kurtzz <przemyslaw.kurc@gmail.com>
2016-11-28 17:39:02 +0100

    Refactoring

--- zadanie3/src/main/java/pl/edu/agh/amwj/value/SValue.java
+++ zadanie3/src/main/java/pl/edu/agh/amwj/value/SValue.java
@@ -5,7 +5,6 @@ package pl.edu.agh.amwj.value;
  */
 public class SValue extends HeapValue {
     private StringValue content;
-//    private int heapIndex;
 
     public SValue(StringValue content) {
         this.content = content;
@@ -23,14 +22,6 @@ public class SValue extends HeapValue {
         this.content = content;
     }
 
-//    public int getHeapIndex() {
-//        return heapIndex;
-//    }
-//
-//    public void setHeapIndex(int heapIndex) {
-//        this.heapIndex = heapIndex;
-//    }
-
     public Value evaluate() {
         return this;
     }
@@ -39,23 +30,23 @@ public class SValue extends HeapValue {
         return 0;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        SValue sValue = (SValue) o;
-
-        return content != null ? content.equals(sValue.content) : sValue.content == null;
-    }
-
-    @Override
-    public int hashCode() {
-        return content != null ? content.hashCode() : 0;
-    }
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (o == null || getClass() != o.getClass()) return false;
+//
+//        SValue sValue = (SValue) o;
+//
+//        return content != null ? content.equals(sValue.content) : sValue.content == null;
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        return content != null ? content.hashCode() : 0;
+//    }
 
     @Override
     public String toString() {
-        return content == null ? "null" : content.toString();
+        return content == null ? "NULL" : content.toString();
     }
 }
--- zadanie3/src/main/java/pl/edu/agh/amwj/value/TValue.java
+++ zadanie3/src/main/java/pl/edu/agh/amwj/value/TValue.java
@@ -7,7 +7,6 @@ public class TValue extends HeapValue{
     private TValue f1;
     private TValue f2;
     private IntegerValue data;
-//    private int heapIndex;
 
     public TValue(TValue f1, TValue f2, IntegerValue data) {
         this.f1 = f1;
@@ -39,14 +38,6 @@ public class TValue extends HeapValue{
         this.data = data;
     }
 
-//    public int getHeapIndex() {
-//        return heapIndex;
-//    }
-//
-//    public void setHeapIndex(int heapIndex) {
-//        this.heapIndex = heapIndex;
-//    }
-
     public Value evaluate() {
         return this;
     }
@@ -55,13 +46,13 @@ public class TValue extends HeapValue{
         return 0;
     }
 
-    @Override
-    public String toString() {
-
-        return "TValue{" +
-                "f1=" + f1 +
-                ", f2=" + f2 +
-                ", data=" + data +
-                '}';
-    }
+//    @Override
+//    public String toString() {
+//
+//        return "TValue{" +
+//                "f1=" + f1 +
+//                ", f2=" + f2 +
+//                ", data=" + data +
+//                '}';
+//    }
 }
